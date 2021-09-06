3d6a8476b6f4363ad2b78e6046bd253e4c22beac
Vignesh <vigneshnadar4619@gmail.com>
2021-09-06 22:45:17 +0530

    basic working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { callbackify } from 'util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DailyAffirmations';
+  affirmationsData;
+  affirmation;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    let res = this.http.get('https://dulce-affirmations-api.herokuapp.com/affirmation/index');
+    res.subscribe(data => {
+      this.affirmationsData = data
+      console.log(data);
+      this.getRandomAffirmations()
+    })
+
+  }
+
+  getRandomAffirmations() {
+    let randomIndex = Math.floor(Math.random() * this.affirmationsData.length)
+
+    // console.log(this);
+    // console.log(this.affirmationsData[randomIndex].phrase)
+
+    this.affirmation = this.affirmationsData[randomIndex].phrase
+
+    let appComponentObj = this
+    setInterval(function () { appComponentObj.getRandomAffirmations(); }, 3000);
+  }
+
 }
